4818dda1af6a46090eb9c0833ace325a81ff5815
Calum <calumcmccall@gmail.com>
2022-09-05 15:06:59 +0100

    TestCalculator file setup to start writing tests

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,2 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestCalculator {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+
+    }
+
 }
