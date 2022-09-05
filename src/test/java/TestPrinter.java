8f9dba8470b65764e36cef8a346f5fb97bad5764
Calum <calumcmccall@gmail.com>
2022-09-05 15:43:00 +0100

    printer class

---
+++ src/test/java/TestPrinter.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPrinter {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(50);
+    }
+
+    @Test
+    public void hasSheetsLeft(){
+        assertEquals(50, printer.getSheetsLeft());
+    }
+
+}
