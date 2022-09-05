220e1f123a75403dbf120e2ae37b60585f995c73
Calum <calumcmccall@gmail.com>
2022-09-05 15:28:22 +0100

    WaterBottle has a volume

--- src/test/java/TestWaterBottle.java
+++ src/test/java/TestWaterBottle.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestWaterBottle {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle();
+    }
+
+    @Test
+    public void volumeStartsAt100(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
 }
